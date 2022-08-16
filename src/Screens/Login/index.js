f7e7143c792d8548e6e25c0a464b46a76d148818
gabriel <gr49168@gmail.com>
2022-08-16 11:03:59 -0300

    autenticacao

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import firebase from "../../config/firebase";
 import { View, Text, TouchableOpacity, Image, TextInput, SafeAreaView} from 'react-native';
 import Styles from './styles';
 
 
-export default props => (
+export default function Login({navigation}){
+    const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [errorLogin, setErrorLogin] = useState("");
+        const loginFirebase = ()=>{
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            let user = userCredential.user;
+            navigation.navigate("Home", {idUser: user.uid})
+        })
+        .catch((error) => {
+            setErrorLogin(true)
+            let errorCode = error.code;
+            let errorMessage = error.message;
+        });
+}
 
+    return(
+        
     <SafeAreaView  style={Styles.container}>
-           <Image source={require('../../assets/Imagens/logo.png')} style={Styles.logo} />
+    <Image source={require('../../assets/Imagens/logo.png')} style={Styles.logo} />
 
-        <Text style={Styles.text}>Login obrigatório para acessar o sistema!</Text>
+ <Text style={Styles.text}>Login obrigatório para acessar o sistema!</Text>
 
-        <TextInput style={Styles.input} placeholder="  Digite seu usuário"  />
-        <TextInput style={Styles.input} secureTextEntry={true} placeholder="  Digite sua senha"  />
+ <TextInput style={Styles.input} placeholder="  Digite seu usuário"  onChangeText={(text) => setEmail(text)}/>
+ <TextInput style={Styles.input} secureTextEntry={true} placeholder="  Digite sua senha"  onChangeText={(text) => setPassword(text)} />
 
-        <TouchableOpacity style={Styles.btn} onPress={() => props.navigation.navigate('Home')} >
-            <Text style={Styles.textbtn} > Login </Text>
-        </TouchableOpacity>
-    </SafeAreaView>
-)
+ <TouchableOpacity style={Styles.btn} onPress={loginFirebase}>
+     <Text style={Styles.textbtn} > Login </Text>
+ </TouchableOpacity>
+</SafeAreaView>
+
+    )
+}
